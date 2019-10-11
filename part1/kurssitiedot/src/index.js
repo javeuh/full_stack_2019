ba028ce584f1e0987d9396fcb7078958e0f772fd
Jani Veihtola <jani.veihtola@gmail.com>
2019-10-11 20:02:16 +0300

    osa1, kurssitiedot 1.2

---
+++ part1/kurssitiedot/src/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = props => <h1>{ props.course }</h1>;
+
+const Part = props => (
+  <p>
+    { props.part } { props.exercises }
+  </p>
+);
+
+const Content = props => (
+  <div>
+    <Part part={ props.part[0] } exercises={ props.exercises[0] } />
+    <Part part={ props.part[1] } exercises={ props.exercises[1] } />
+    <Part part={ props.part[2] } exercises={ props.exercises[2] } />
+  </div>
+);
+
+const Total = props => {
+    let total = 0;
+    props.exercises.forEach(exercise => {
+        total += exercise
+    });
+    return (
+        <p>Number of exercises { total }</p>
+    );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={ course } />
+      <Content
+        part={ [part1, part2, part3] }
+        exercises={ [exercises1, exercises2, exercises3] }
+      />
+      <Total exercises={  [exercises1, exercises2, exercises3] } />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
